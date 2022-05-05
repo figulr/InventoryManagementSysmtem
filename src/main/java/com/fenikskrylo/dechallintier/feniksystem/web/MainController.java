d544c53c7535414885996a44a9edc0cd5a9b97de
figulr <dechallintier@gmail.com>
2022-05-05 12:45:36 +0900

    @LoginUser를 통한 세션관리 기능 구현

--- src/main/java/com/fenikskrylo/dechallintier/feniksystem/web/MainController.java
+++ src/main/java/com/fenikskrylo/dechallintier/feniksystem/web/MainController.java
@@ -1,23 +1,20 @@
 package com.fenikskrylo.dechallintier.feniksystem.web;
 
+import com.fenikskrylo.dechallintier.feniksystem.config.auth.LoginUser;
 import com.fenikskrylo.dechallintier.feniksystem.config.auth.dto.SessionUser;
+import com.fenikskrylo.dechallintier.feniksystem.service.ProductsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class MainController {
 
-    private final HttpSession httpSession;
-
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user!=null){
             model.addAttribute("userName", user.getName());
         }
